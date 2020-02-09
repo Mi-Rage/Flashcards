352052347e50ffa5b9e7eef82d9618a2b70cde59
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-02-09 15:55:22 +0300

    Add files via upload
    Новая версия программы Flashcards. Теперь с проверкой на существующие поля на этапе создания карточек и на этапе ответов на вопросы.
    По заданию HyperSkill:
    Imagine a situation: the answer is wrong for the given term, but it is correct for another term. Let's consider situations like this.
    
    You can use maps. Ask all card's definitions in the order of addition. If the definition is wrong for the current term but it is correct for another, output the original term.
    
    When the user tries to add a duplicated term or definition, forbid it and ask again until the user inputs a unique one. For now, you are able to implement this without a try catch construction. Use the rule: if you can avoid exception-based logic, avoid it!
    
    PS Добавлена проверка на этапе ввода к-ва создаваемых карточек на предмет ошибочного ввода символов.

--- Card.java
+++ Card.java
@@ -1,42 +1,87 @@
 package flashcards;
 
+
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Card {
-    private static int quantity;
-    private GameCards[] gameCard;
+    Map<String, String> card = new TreeMap<>();
     Scanner scanner = new Scanner(System.in);
 
-    public void initCard(){
+    public void initCard() {
+        String term;
+        String definition;
 
-        System.out.print("Input the number of cards:\n>");
-        quantity = scanner.nextInt();
-        gameCard = new GameCards[quantity];
-        scanner.nextLine();
-        for (int i = 0; i < quantity; i++) {
-            System.out.print("The card #" + i + ">");
-            String term = scanner.nextLine();
-            System.out.print("The definition of the card #" + i + ">");
-            String definition = scanner.nextLine();
-            gameCard[i] = new GameCards(term, definition);
-            System.out.println(gameCard[i].getTerm() + " " + gameCard[i].getDefinition());
-        }
-    }
-
-    public static int getQuantity(){
-        return quantity;
-    }
-
-    public void gamePlay() {
-        String answer;
-        for (int i = 0; i < Card.getQuantity(); i++) {
-            System.out.print("Print the definition of \"" + gameCard[i].getTerm() + "\"\n>");
-            answer = scanner.nextLine();
-            if (answer.equals(gameCard[i].getDefinition())) {
-                System.out.println("Correct answer");
-            } else {
-                System.out.println("Wrong answer. The correct one is \"" + gameCard[i].getDefinition() + "\"");
+        int quantity;
+        while(true){
+            try {
+                System.out.print("Input the number of cards:\n>");
+                quantity = scanner.nextInt();
+                break;
+            } catch (Exception e) {
+                System.out.println("ERROR: Enter only whole numbers! Try again!");
+            }
+            finally {
+                scanner.nextLine();
             }
         }
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.print("The card #" + i + "\n>");
+            do {
+                term = scanner.nextLine();
+                if (isContainsKey(term)) {
+                    System.out.println("The card \"" + term + "\" already exists. Try again:");
+                }
+            } while (isContainsKey(term));
+
+            do {
+                System.out.print("The definition of the card #" + i + "\n>");
+                definition = scanner.nextLine();
+                if (isContainsDef(definition)) {
+                    System.out.print("The definition \"" + definition + "\" already exists.");
+                }
+            } while (card.containsValue(definition));
+            card.put(term, definition);
+        }
     }
+
+    // Does the map have a "key" value ?
+    public boolean isContainsKey(String term){
+        return card.containsKey(term);
+    }
+
+    // Does the map have a "definition" value ?
+    public boolean isContainsDef(String definition){
+        return card.containsValue(definition);
+    }
+
+
+    public void gamePlay(){
+        String answer;
+        for (var entry : card.entrySet()) {
+            System.out.print("Print the definition of \"" + entry.getKey() + "\"\n>");
+            answer = scanner.nextLine();
+            if(answer.equals(entry.getValue())) {
+                System.out.println("Correct answer.");
+            } else {
+                System.out.print("Wrong answer. The correct one is \"" + entry.getValue() + "\"");
+                if (card.containsValue(answer)) {
+                    for (var find : card.entrySet()) {
+                        if (find.getValue().equals(answer)){
+                            System.out.println(", you've just written the definition of \"" + find.getKey() + "\".");
+                            break;
+                        }
+                    }
+                }  else {
+                    System.out.println(".");
+                }
+            }
+        }
+
+    }
+
+
+
 }
