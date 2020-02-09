5ca40a324909dce685f16d41bd40f88d933d84ef
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-02-09 20:29:59 +0300

    Изменен алгоритм вывода ответов и создан метод getKeyFromMap.
    Изменен алгоритм вывода ответов на карты.
    Создан и использован метод getKeyFromMap(), позволяющий получить key в элементе Map по его value. Для использовании в ответах на вопросы.

--- Card.java
+++ Card.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Card {
-    Map<String, String> card = new TreeMap<>();
+    Map<String, String> cards = new TreeMap<>();
     Scanner scanner = new Scanner(System.in);
 
     public void initCard() {
@@ -14,21 +14,20 @@ public class Card {
         String definition;
 
         int quantity;
-        while(true){
+        while (true) {
             try {
-                System.out.print("Input the number of cards:\n>");
+                System.out.println("Input the number of cards:");
                 quantity = scanner.nextInt();
                 break;
             } catch (Exception e) {
                 System.out.println("ERROR: Enter only whole numbers! Try again!");
-            }
-            finally {
+            } finally {
                 scanner.nextLine();
             }
         }
 
         for (int i = 0; i < quantity; i++) {
-            System.out.print("The card #" + i + "\n>");
+            System.out.println("The card #" + i);
             do {
                 term = scanner.nextLine();
                 if (isContainsKey(term)) {
@@ -36,52 +35,53 @@ public class Card {
                 }
             } while (isContainsKey(term));
 
+            System.out.println("The definition of the card #" + i + ":");
             do {
-                System.out.print("The definition of the card #" + i + "\n>");
                 definition = scanner.nextLine();
                 if (isContainsDef(definition)) {
-                    System.out.print("The definition \"" + definition + "\" already exists.");
+                    System.out.println("The definition \"" + definition + "\" already exists. Try again:");
                 }
-            } while (card.containsValue(definition));
-            card.put(term, definition);
+            } while (cards.containsValue(definition));
+            cards.put(term, definition);
         }
     }
 
     // Does the map have a "key" value ?
-    public boolean isContainsKey(String term){
-        return card.containsKey(term);
+    public boolean isContainsKey(String term) {
+        return cards.containsKey(term);
     }
 
     // Does the map have a "definition" value ?
-    public boolean isContainsDef(String definition){
-        return card.containsValue(definition);
+    public boolean isContainsDef(String definition) {
+        return cards.containsValue(definition);
     }
 
-
-    public void gamePlay(){
-        String answer;
-        for (var entry : card.entrySet()) {
-            System.out.print("Print the definition of \"" + entry.getKey() + "\"\n>");
-            answer = scanner.nextLine();
-            if(answer.equals(entry.getValue())) {
-                System.out.println("Correct answer.");
-            } else {
-                System.out.print("Wrong answer. The correct one is \"" + entry.getValue() + "\"");
-                if (card.containsValue(answer)) {
-                    for (var find : card.entrySet()) {
-                        if (find.getValue().equals(answer)){
-                            System.out.println(", you've just written the definition of \"" + find.getKey() + "\".");
-                            break;
-                        }
-                    }
-                }  else {
-                    System.out.println(".");
-                }
+    // Get key from map by value
+    public <K, V> K getKeyFromMap(Map<K, V> card, V description) {
+        for (Map.Entry<K, V> entry : card.entrySet()) {
+            if (entry.getValue().equals(description)) {
+                return entry.getKey();
             }
         }
-
+        return null;
     }
 
 
+    public void gamePlay() {
+        String answer;
 
+        for (var entry : cards.entrySet()) {
+            System.out.println("Print definition of \"" + entry.getKey() + "\":");
+            answer = scanner.nextLine();
+            if (!entry.getValue().equals(answer) && cards.containsValue(answer)) {
+                System.out.println("Wrong answer. The correct one is \"" + entry.getValue()
+                        + "\", you've just written the definition of \""
+                        + getKeyFromMap(cards, answer) + "\".");
+            } else if (entry.getValue().equals(answer)) {
+                System.out.println("Correct answer.");
+            } else {
+                System.out.println("Wrong answer. The correct one is \"" + entry.getValue() + "\"");
+            }
+        }
+    }
 }
