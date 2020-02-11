ca9e072af811fff78d0692a77c7fddc3fe83a6a9
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-02-11 21:36:45 +0300

    Добавлено меню, изменены методы add и ask
    Добавлено меню с выбором режимов.
    Изменен метод добавления карточке согласно ТЗ.
    Изменен метод отгадывания карточек согласно ТЗ.
    Переделана проверка к-ва отгадываемых карточек на ввод символов а не цифр с контролем к-ва созданных карт.

--- Card.java
+++ Card.java
@@ -9,42 +9,53 @@ public class Card {
     Map<String, String> cards = new TreeMap<>();
     Scanner scanner = new Scanner(System.in);
 
-    public void initCard() {
+    public void playGame() {
+        String action;
+        do {
+            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            action = scanner.next();
+            scanner.nextLine();
+            switch (action) {
+                case "add":
+                    addCard();
+                    break;
+                case "remove":
+                    // remove a cerd
+                case "import":
+                    // imoprt file
+                case "export":
+                    // save file
+                case "ask":
+                    gamePlay();
+
+            }
+        } while (!action.equals("exit"));
+
+    }
+
+    public void addCard() {
         String term;
         String definition;
 
-        int quantity;
-        while (true) {
-            try {
-                System.out.println("Input the number of cards:");
-                quantity = scanner.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.println("ERROR: Enter only whole numbers! Try again!");
-            } finally {
-                scanner.nextLine();
+
+        System.out.println("The card :");
+        do {
+            term = scanner.nextLine();
+            if (isContainsKey(term)) {
+                System.out.printf("The card \"%s\" already exists. Try again:%n", term);
             }
-        }
+        } while (isContainsKey(term));
 
-        for (int i = 0; i < quantity; i++) {
-            System.out.println("The card #" + i);
-            do {
-                term = scanner.nextLine();
-                if (isContainsKey(term)) {
-                    System.out.println("The card \"" + term + "\" already exists. Try again:");
-                }
-            } while (isContainsKey(term));
-
-            System.out.println("The definition of the card #" + i + ":");
-            do {
-                definition = scanner.nextLine();
-                if (isContainsDef(definition)) {
-                    System.out.println("The definition \"" + definition + "\" already exists. Try again:");
-                }
-            } while (cards.containsValue(definition));
-            cards.put(term, definition);
-        }
+        System.out.println("The definition of the card :");
+        do {
+            definition = scanner.nextLine();
+            if (isContainsDef(definition)) {
+                System.out.printf("The definition \"%s\" already exists. Try again:%n", definition);
+            }
+        } while (cards.containsValue(definition));
+        cards.put(term, definition);
     }
+
 
     // Does the map have a "key" value ?
     public boolean isContainsKey(String term) {
@@ -69,18 +80,36 @@ public class Card {
 
     public void gamePlay() {
         String answer;
+        int quantity;
+
+        do {
+            while (true) {
+                try {
+                    System.out.println("How many times to ask?");
+                    quantity = scanner.nextInt();
+                    break;
+                } catch (Exception e) {
+                    System.out.println("ERROR: Enter only whole numbers! Try again!");
+                } finally {
+                    scanner.nextLine();
+                }
+            }
+            if (quantity > cards.size()) {
+                System.out.printf("ERROR: %s times is more than %s cards! Try again!%n" , quantity , cards.size());
+            }
+        } while (quantity > cards.size());
+
 
         for (var entry : cards.entrySet()) {
-            System.out.println("Print definition of \"" + entry.getKey() + "\":");
+            System.out.printf("Print definition of \"%s\":%n", entry.getKey());
             answer = scanner.nextLine();
             if (!entry.getValue().equals(answer) && cards.containsValue(answer)) {
-                System.out.println("Wrong answer. The correct one is \"" + entry.getValue()
-                        + "\", you've just written the definition of \""
-                        + getKeyFromMap(cards, answer) + "\".");
+                System.out.printf("Wrong answer. The correct one is \"%s\", you've just written the definition of \"%s\".%n"
+                        , entry.getValue(), getKeyFromMap(cards, answer));
             } else if (entry.getValue().equals(answer)) {
                 System.out.println("Correct answer.");
             } else {
-                System.out.println("Wrong answer. The correct one is \"" + entry.getValue() + "\"");
+                System.out.printf("Wrong answer. The correct one is \"%s\".%n", entry.getValue());
             }
         }
     }
